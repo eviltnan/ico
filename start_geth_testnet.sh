42b70f6fc5b229db454e5e59e2e0241ef41a71d3
Sergey Cheparev <sergei.cheparev@gmail.com>
2017-11-26 20:13:27 +0100

    Start geth and deployment scripts for test token

--- start_geth_testnet.sh
+++ start_geth_testnet.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-geth --testnet --rpc --rpcport 8546 --rpcaddr 127.0.0.1 --rpccorsdomain "*" --rpcapi "eth,web3" --unlock 0x0DBc7F3504bc285F24816D1b50DeEE8B9C1E116f --networkid 3
+geth --testnet --rpc --rpcport 8546 --rpcaddr 127.0.0.1 --rpccorsdomain "*" --rpcapi "eth,web3" --unlock 0x0dbc7f3504bc285f24816d1b50deee8b9c1e116f --networkid 3 --etherbase 0x0dbc7f3504bc285f24816d1b50deee8b9c1e116f
+
 
